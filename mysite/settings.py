5c93a4c91063c118904eb929d856478931abbdd4
YasserDbeis <ydbeis@asu.edu>
2020-05-05 17:05:13 -0700

    heroku fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal300'
+# GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal300'
 # django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
