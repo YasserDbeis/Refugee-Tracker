e573e02dfa9252ced9616b3887f13c3578577faf
YasserDbeis <ydbeis@asu.edu>
2020-05-06 02:50:27 -0700

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -158,5 +158,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # django_heroku.settings(locals())
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
