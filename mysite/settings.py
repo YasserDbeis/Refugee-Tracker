ff2dcef1bc9e40d5984fdfeb6ce6f42931d7182b
YasserDbeis <ydbeis@asu.edu>
2020-05-06 01:29:36 -0700

    heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,8 +103,8 @@ DATABASES = {
 
 # db_from_env = dj_database_url.config()
 # DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+# DATABASES['default'] = dj_database_url.config()
+# DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # DATABASES['default'] =  dj_database_url.config()
 # DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
