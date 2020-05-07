17fd332dfe2e820c16118304fd9e4219101b3109
YasserDbeis <ydbeis@asu.edu>
2020-05-07 03:39:39 -0700

    heresf

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 import django_heroku
 from os import environ
-GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal300'
+# GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal300'
 # django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,12 +116,12 @@ DATABASES['default']['ENGINE'] = "django.contrib.gis.db.backends.postgis"
 # GDAL_LIBRARY_PATH = '/app/.geodjango/gdal/lib/libgdal.so'
 #help me god
 
-# _GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH', None)
-# if _GDAL_LIBRARY_PATH:
-#     GDAL_LIBRARY_PATH = _GDAL_LIBRARY_PATH
-# _GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH', None)
-# if _GEOS_LIBRARY_PATH:
-#     GEOS_LIBRARY_PATH = _GEOS_LIBRARY_PATH
+_GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH', None)
+if _GDAL_LIBRARY_PATH:
+    GDAL_LIBRARY_PATH = _GDAL_LIBRARY_PATH
+_GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH', None)
+if _GEOS_LIBRARY_PATH:
+    GEOS_LIBRARY_PATH = _GEOS_LIBRARY_PATH
 
 # DATABASES['default] = {default': dj_database_url.config()}
 # db_from_env = dj_database_url.config()
