cf1564c80dcb486c14a3a0f0eaf2bbf104102cbf
YasserDbeis <ydbeis@asu.edu>
2020-05-08 03:34:11 -0700

    stylistic changes - converted for refugees

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'maps',
     'django.contrib.gis',
+
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    "mysite\maps\static"
+]
