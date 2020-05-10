62b1feda013789585ab53b856a5399f4a6b58a82
YasserDbeis <ydbeis@asu.edu>
2020-05-10 14:31:43 -0700

    added articles and stylistic changes

--- maps/views.py
+++ maps/views.py
@@ -5,10 +5,14 @@ from django.template import loader, RequestContext
 from django.core.serializers import serialize
 from .models import Location
 from djgeojson.serializers import Serializer as GeoJSONSerializer
-from pynytimes import NYTAPI
-
+import ast
 import json
 from django.contrib.gis.geos import Point
+import datetime
+from pynytimes import NYTAPI
+
+nyt = NYTAPI("n0zstttYijH85qvE5B25zpdcY6VCoAW0")
+
 # Create your views here.
 
 def index(request):
@@ -32,7 +36,8 @@ def index(request):
     linksJson = json.dumps(links)
     print(linksJson)
     latest_locations = Location.objects.order_by('city')
-    geo_json_string = GeoJSONSerializer().serialize(Location.objects.all(), use_natural_keys=True, with_modelname=False)
+    geo_json_string = GeoJSONSerializer().serialize(Location.objects.all(),  use_natural_keys=True, with_modelname=False)
+
 
     # geo_dict = json.load(geo_json_string)
     # print(type(geo_dict))
@@ -43,27 +48,11 @@ def index(request):
     # print(geo_json_string)
     template = loader.get_template('maps/index.html')
     context = {
-        'geo_json_string': geo_json_string,
-        'links': linksJson,
+        'geo_json_string' : geo_json_string,
+        'links' : linksJson,
     }
     # output = ", ".join(l.city for l in latest_locations)
-    return render(request, "maps/index.html", {'geo_json_string': geo_json_string, 'links': linksJson})
-
-
-
-
-
-    # latest_locations = Location.objects.order_by('city')
-    # geo_json_string = GeoJSONSerializer().serialize(Location.objects.all(),  use_natural_keys=True, with_modelname=False)
-    #
-    # # geo_json_string = (GeoJSONSerializer().('geojson', Location.objects.all(), geometry_field = 'geom', fields = ('city', 'state', 'reference')))
-    # print(geo_json_string)
-    # template = loader.get_template('maps/index.html')
-    # context = {
-    #     'geo_json_string' : geo_json_string
-    # }
-    # # output = ", ".join(l.city for l in latest_locations)
-    # return render(request, "maps/index.html", {'geo_json_string' : geo_json_string})
+    return render(request, "maps/index.html", {'geo_json_string' : geo_json_string, 'links' : linksJson})
 
 def detail(request, location_id):
     return HttpResponse("Location ID is {}".format(location_id))
