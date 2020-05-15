53baf93be02032bf0a3f5589f5724c6922a8cc57
YasserDbeis <ydbeis@asu.edu>
2020-05-14 17:10:18 -0700

    added statistics and figures

--- maps/views.py
+++ maps/views.py
@@ -3,15 +3,19 @@ from django.http import HttpResponse
 from .models import Location
 from django.template import loader, RequestContext
 from django.core.serializers import serialize
-from .models import Location
+from .models import Location, Population
 from djgeojson.serializers import Serializer as GeoJSONSerializer
 import ast
 import json
 from django.contrib.gis.geos import Point
-import datetime
+from django.db.models import Sum
 from pynytimes import NYTAPI
 
 nyt = NYTAPI("n0zstttYijH85qvE5B25zpdcY6VCoAW0")
+
+import datetime
+
+
 
 # Create your views here.
 
@@ -35,24 +39,50 @@ def index(request):
 
     linksJson = json.dumps(links)
     print(linksJson)
-    latest_locations = Location.objects.order_by('city')
     geo_json_string = GeoJSONSerializer().serialize(Location.objects.all(),  use_natural_keys=True, with_modelname=False)
 
+    regionsQuery = Population.objects.values_list("region", flat=True).order_by("region").distinct()
+    regionsList = list(regionsQuery)
 
-    # geo_dict = json.load(geo_json_string)
-    # print(type(geo_dict))
-    # for r in geo_dict:
-    #     print(r['city'])
+    regionJSONs = []
 
-    # geo_json_string = (GeoJSONSerializer().('geojson', Location.objects.all(), geometry_field = 'geom', fields = ('city', 'state', 'reference')))
-    # print(geo_json_string)
-    template = loader.get_template('maps/index.html')
-    context = {
-        'geo_json_string' : geo_json_string,
-        'links' : linksJson,
-    }
-    # output = ", ".join(l.city for l in latest_locations)
-    return render(request, "maps/index.html", {'geo_json_string' : geo_json_string, 'links' : linksJson})
+    for region in regionsList:
+        regionsPop = {}
+        regionsPop['region'] = region
+        regionsPop['population'] = (Population.objects.filter(region=region).aggregate(Sum('refugeePop')))['refugeePop__sum']
+        regionJSONs.append(regionsPop)
+
+    regionsPop = json.dumps(regionJSONs)
+
+    subregionsQuery = Population.objects.values_list("subRegion", flat=True).order_by("subRegion").distinct()
+    subregionsList = list(subregionsQuery)
+
+    subregionsJSONs = []
+
+    for subregion in subregionsList:
+        subregionsPop = {}
+        subregionsPop['subregion'] = subregion
+        subregionsPop['population'] = (Population.objects.filter(subRegion=subregion).aggregate(Sum('refugeePop')))['refugeePop__sum']
+        subregionsJSONs.append(subregionsPop)
+
+    subregionsPop = json.dumps(subregionsJSONs)
+
+    countriesQuery = Population.objects.values_list("origin", flat=True).order_by("refugeePop").distinct()
+    countriesList = list(countriesQuery)
+    countriesList = countriesList[-9:]
+    print(countriesList)
+    countriesJSONs = []
+
+    for country in countriesList:
+        countriesPop = {}
+        countriesPop['country'] = country
+        countriesPop['population'] = (Population.objects.filter(origin=country).aggregate(Sum('refugeePop')))['refugeePop__sum']
+        countriesJSONs.append(countriesPop)
+
+
+    countriesPop = json.dumps(countriesJSONs)
+
+    return render(request, "maps/index.html", {'geo_json_string' : geo_json_string, 'links' : linksJson, 'regions' : regionsPop, 'subregions' : subregionsPop, 'countries' : countriesPop})
 
 def detail(request, location_id):
     return HttpResponse("Location ID is {}".format(location_id))
