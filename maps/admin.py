af1a1131d7792c1f19a5631aa4ec9b838b8ab4d5
YasserDbeis <ydbeis@asu.edu>
2020-05-14 17:28:37 -0700

    heroku

--- maps/admin.py
+++ maps/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Location
+from .models import Location, Population
 
 admin.site.register(Location)
+admin.site.reigster(Population)
