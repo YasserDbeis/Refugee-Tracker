39540e6c89a2efe1936b9fe1b1e17563dd8fb4bc
YasserDbeis <ydbeis@asu.edu>
2021-08-18 17:16:40 -0700

    reverted changes back in

--- maps/views.py
+++ maps/views.py
@@ -1,3 +1,4 @@
+from asgiref.sync import sync_to_async
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Location
@@ -9,69 +10,84 @@ import ast
 import json
 from django.contrib.gis.geos import Point
 from django.db.models import Sum
-from pynytimes import NYTAPI
 import logging
 from datetime import datetime, timedelta
 from newsapi import NewsApiClient
 import flickr_api
 from flickr_api import Walker, Photo
-nyt = NYTAPI("n0zstttYijH85qvE5B25zpdcY6VCoAW0")
-flickr_api.set_keys(api_key = '1e68e420d9c8bd0e33301429630cf3b5', api_secret = '67a16ddaf7414f8f')
-
 import datetime
+import os, sys
+from pygooglenews import GoogleNews
+from gnews import GNews
+import asyncio
+import aiohttp
 
+# sys.path.insert(1, '/path/to/application/app/folder')
 
+sys.path.append(os.path.realpath('.'))
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+
+from .keys import NEWSAPI_KEY
+from .keys import FLICKRAPI_KEY
+from .keys import FLICKRAPI_SECRET
+from .keys import MAPBOX_KEY
+
+MAPBOX_KEY = json.dumps(MAPBOX_KEY)
+
+newsapi = NewsApiClient(api_key=NEWSAPI_KEY)
+flickr_api.set_keys(api_key = FLICKRAPI_KEY, api_secret = FLICKRAPI_SECRET)
 
 # Create your views here.
 
-def index(request):
+def getArticles():
 
-    geo_json_string = GeoJSONSerializer().serialize(Location.objects.all(),  use_natural_keys=True, with_modelname=False)
-
-
-
-    querySet = [("M'bera Refugee Camp", 'MR'), ("Burkina Faso Refugee Camp", 'BF'), ("Niger Refugee Camp", 'NE'),
-                ("Niger Refugee Camp", 'NE'), ("Malawi Refugee Camp", 'MW'),
-                ("Kenya Refugee Camp", 'KY'), ("Ethiopia Refugee Camp", 'ET'), ("Syria Refugee Camp", 'SY'),
-                ("Jordan Refugee Camp", 'JO'), ("Iraq Refugee Camp", 'IQ'),
-                ("Bangladesh Refugee Camp", 'BD'), ("Sudan Refugee Camp", 'SD'), ("Turkey Refugee Camp", 'TR'),
-                ("West Nile Refugee Camp", 'WN')]
-
-    newsapi = NewsApiClient(api_key='e7574739bccf4313bf4f01f06774802f')
+    regionAndCountries = [
+        ('Middle East', ['SY', 'TR', 'JO', 'IQ']),
+        ('East Africa', ['KY', 'ET', 'SD', 'WN', 'MW']),
+        ('West Africa', ['MR', 'BF', 'NE',]),
+        ('South Asia', ['BD'])
+    ]
 
     links = []
-    for query, iso2 in querySet:
+    for region, countries in regionAndCountries:
+  
+        gn = GNews(max_results=20, language='en')
     
-        all_articles = newsapi.get_everything(q=query,
-                                              from_param=datetime.datetime.now().date() - timedelta(days=29),
-                                              to=datetime.datetime.now().date(),
-                                              language='en',
-                                              sort_by='relevancy',
-                                              page=1)
+        all_articles = gn.get_news(region + " Refugee Camp")
+        print(all_articles)
+
         countryToList = {}
     
-        if (all_articles['totalResults'] is 0):
-            all_articles = newsapi.get_everything(q='Refugee Camps',
-                                                  from_param=datetime.datetime.now().date() - timedelta(days=29),
-                                                  to=datetime.datetime.now().date(),
-                                                  language='en',
-                                                  sort_by='relevancy',
-                                                  page=1)
+        if (len(all_articles) == 0):
+                all_articles = gn.get_news('Refugee Camps')
+                    
         subList = []
-        for article in all_articles['articles']:
+        i = 0
+        for article in all_articles:
             info = {'url': (article['url']),
                     'headline': (str(article['title']).replace(u"\u2018", "'").replace(u"\u2019", "'"))}
             subList.append(info)
+            i = i + 1
+            if(i == 20):
+                break
     
-        countryToList[iso2] = subList
+        for country in countries:
+            countryToList[country] = subList
+
         links.append(countryToList)
 
     linksJson = json.dumps(links)
 
-    # print(linksJson)
+    return linksJson
+    
 
+def index(request):
+
+    geo_json_string = GeoJSONSerializer().serialize(Location.objects.all(), use_natural_keys=True, with_modelname=False)
+
+    links = getArticles()
+    
     #end of articles - start of regional stats queries
-
 
     regionsQuery = Population.objects.values_list("region", flat=True).order_by("region").distinct()
     regionsList = list(regionsQuery)
@@ -102,7 +118,6 @@ def index(request):
     countriesQuery = Population.objects.values_list("origin", flat=True).order_by("refugeePop").distinct()
     countriesList = list(countriesQuery)
     countriesList = countriesList[-9:]
-    # print(countriesList)
     countriesJSONs = []
 
     for country in countriesList:
@@ -111,41 +126,14 @@ def index(request):
         countriesPop['population'] = (Population.objects.filter(origin=country).aggregate(Sum('refugeePop')))['refugeePop__sum']
         countriesJSONs.append(countriesPop)
 
-
     countriesPop = json.dumps(countriesJSONs)
 
+    # hardcoded image urls because API was too slow
+    # user = flickr_api.Person.findByUserName('MedGlobal')
+    # photos = user.getPhotos()
 
-
-    # instantiate PyUnsplash object
-
-    # pyunsplash logger defaults to level logging.ERROR
-    # If you need to change that, use getLogger/setLevel
-    # on the module logger, like this:
-
-    # Start with the generic collection, maximize number of items
-    # note: this will run until all photos of all collections have
-    #       been visited, unless a connection error occurs.
-    #       Typically the API hourly limit gets hit during this
-
-    user = flickr_api.Person.findByUserName('MedGlobal')
-    photos = user.getPhotos()
-
-    images = []
-    i = 0
-    for photo in photos:
-        i+=1
-        linkAndAuthor = {'link': photo.getPhotoFile(), 'title': photo.title}
-        images.append(linkAndAuthor)
-        if i == 20:
-            break
-
+    images = ['https://live.staticflickr.com/65535/49757472791_95d370c625_b.jpg', 'https://live.staticflickr.com/65535/49757808382_4b4e4a86d4_b.jpg', "https://live.staticflickr.com/65535/49757474531_155b81172d_6k.jpg"]     
     images = json.dumps(images)
 
-    return render(request, "maps/index.html", {'geo_json_string' : geo_json_string, 'links' : linksJson, 'regions' : regionsPop, 'subregions' : subregionsPop, 'countries' : countriesPop, 'images' : images})
-
-# def stories(request):
-
-
-
-#     return render(request, "maps/index.html", {'images' : images})
-
+    print("IMAGES DONE")
+    return render(request, "maps/index.html", {'geo_json_string' : geo_json_string, 'links' : links, 'regions' : regionsPop, 'subregions' : subregionsPop, 'countries' : countriesPop, 'images' : images, 'MAPBOX_KEY' : MAPBOX_KEY})
